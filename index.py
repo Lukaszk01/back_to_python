03f488a5f9a64953ef17723b5d476da1bf756e0a
Lukasz <lukaszkkoziel@gmail.com>
2020-06-26 14:37:49 +0900

    Functions

--- index.py
+++ index.py
@@ -88,3 +88,74 @@ print now
 print '%02d/%02d/%04d' % (now.month, now.day, now.year)
 
 print '%02d/%02d/%04d' % (now.month, now.day, now.year) + ' %02d:%02d:%04d' % (now.hour, now.minute, now.second)
+
+def cube(number):
+  return (number * number) * number
+
+def by_three(number):
+  if (number % 3 ==0 ):
+    return cube(number)
+  else:
+    return False
+
+
+    import math
+print math.sqrt(25)
+
+minimum = min(1 , 30, -1, -1000)
+
+print minimum
+maximum = max(1 , 30, -1, -1000)
+
+print maximum
+
+absolute = abs(-42)
+
+print absolute
+
+
+print type(22)
+print type(1.22)
+print type("good")
+
+
+def shut_down(s):
+  if s == "yes":
+    return "Shutting down"
+  elif s == "no":
+    return "Shutdown aborted"
+  else:
+    return "Sorry"
+
+def distance_from_zero(num):
+  if type(num) == int or type(num) == float:
+    return abs(num)
+  else:
+    return "Nope"
+
+
+def hotel_cost(nights):
+  return 140 * nights
+
+def plane_ride_cost(city):
+  if city == "Charlotte":
+    return 183
+  elif city == "Tampa":
+    return 220
+  elif city == "Pittsburgh":
+    return 222
+  elif city == "Los Angeles":
+    return 475
+
+def rental_car_cost(days):
+  cost = days * 40
+  if days >= 7:
+    cost -= 50
+  elif days >= 3:
+    cost -= 20
+  return cost
+
+def trip_cost(city, days, spending_money):
+  return rental_car_cost(days) + hotel_cost(days - 1) + plane_ride_cost(city) + spending_money
+
+print trip_cost("Los Angeles", 5, 600)
