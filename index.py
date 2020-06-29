51a53c065915966a1d80efce1d4dbb04d0057a21
Lukasz <lukaszkkoziel@gmail.com>
2020-06-29 18:00:15 +0900

    python lists and dictionaries

--- index.py
+++ index.py
@@ -42,6 +42,27 @@ product = float_1 * float_2
 big_string = "The product was " + str(product)
 
 
+
+inventory = {
+  'gold' : 500,
+  'pouch' : ['flint', 'twine', 'gemstone'], # Assigned a new list to 'pouch' key
+  'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']
+}
+
+# Adding a key 'burlap bag' and assigning a list to it
+inventory['burlap bag'] = ['apple', 'small ruby', 'three-toed sloth']
+
+# Sorting the list found under the key 'pouch'
+inventory['pouch'].sort()
+
+# Your code here
+inventory['pocket'] = ['seashell', 'strange berry', 'lint']
+
+inventory['backpack'].sort()
+
+inventory['backpack'].remove('dagger')
+inventory['gold'] += 50
+
 skill_completed = "Python Syntax"
 
 exercises_completed = 13
