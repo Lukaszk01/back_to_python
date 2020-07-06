29e24e4a0267d23e62cd38a56667544bb5767d11
Lukasz <lukaszkkoziel@gmail.com>
2020-07-06 18:35:31 +0900

    loops3

--- loops.py
+++ loops.py
@@ -1,16 +1,16 @@
-# from random import randint
-# guesses_left = 3
-# # Start your game!
-# while guesses_left > 0:
-#   random_number = randint(1, 10)
-#   print random_number
-#   guess = int(raw_input("Your guess: "))
-#   if guess == random_number:
-#     print "You win!"
-#     break
-#     guesses_left -= 1
-#   else:
-#     print "You lose"
+from random import randint
+guesses_left = 3
+# Start your game!
+while guesses_left > 0:
+  random_number = randint(1, 10)
+  print random_number
+  guess = int(raw_input("Your guess: "))
+  if guess == random_number:
+    print "You win!"
+    break
+    guesses_left -= 1
+  else:
+    print "You lose"
 
 
 hobbies = []
@@ -18,3 +18,15 @@ hobby = raw_input("Your pick: ")
 for hobby in range(3):
   hobbies.append(hobby)
   print hobbies
+
+
+fruits = ['banana', 'apple', 'orange', 'tomato', 'pear', 'grape']
+
+print 'You have...'
+for f in fruits:
+  if f == 'tomato':
+    print 'A tomato is not a fruit!' # (It actually is.)
+
+  print 'A', f
+else:
+  print 'A fine selection of fruits!'
